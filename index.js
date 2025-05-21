3bd151434449d3ed08a68f0ac1da6e373f040565
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-05-21 19:32:58 +0200

    change js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const changeButton = document.querySelector(".button-change");
 const root = document.querySelector("#root");
 
 const changeBackground = () => {
-  root.style.background = "rgb(238, 240, 169)";
+  root.style.background = "rgb(208, 173, 216)";
 };
 
 changeButton.addEventListener("click", changeBackground);
