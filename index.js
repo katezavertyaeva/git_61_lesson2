1ae7581bf384b1ef787aca196adb354a332ae5d4
Ekaterina Zavertyaeva <katsiarynazavertyaeva@gmail.com>
2025-05-21 19:53:32 +0200

    change text

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const changeButton = document.querySelector('.button-change');
-const changeTitleBtn = document.querySelector('.button-change-title');
-const title = document.querySelector('#title');
-const root = document.querySelector('#root');
+const changeButton = document.querySelector(".button-change");
+const changeTitleBtn = document.querySelector(".button-change-title");
+const title = document.querySelector("#title");
+const root = document.querySelector("#root");
 
 const changeBackground = () => {
-  root.style.background = 'rgb(208, 173, 216)';
+  root.style.background = "rgb(208, 173, 216)";
 };
 
 const changeTitle = () => {
-  title.textContent = 'Git: cloning, resolve conflict';
+  title.textContent = "(Technologies) Git: cloning, resolve conflict";
 };
 
-changeButton.addEventListener('click', changeBackground);
-changeTitleBtn.addEventListener('click', changeTitle);
+changeButton.addEventListener("click", changeBackground);
+changeTitleBtn.addEventListener("click", changeTitle);
