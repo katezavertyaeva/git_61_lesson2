8e64404211ce921f8de3fa2836aeedf9031e1092
Andrey Shemet <an-shem@ukr.net>
2025-05-21 19:50:34 +0200

    add button change title

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const changeButton = document.querySelector(".button-change");
-const root = document.querySelector("#root");
+const changeButton = document.querySelector('.button-change');
+const changeTitleBtn = document.querySelector('.button-change-title');
+const title = document.querySelector('#title');
+const root = document.querySelector('#root');
 
 const changeBackground = () => {
-  root.style.background = "rgb(208, 173, 216)";
+  root.style.background = 'rgb(208, 173, 216)';
 };
 
-changeButton.addEventListener("click", changeBackground);
+const changeTitle = () => {
+  title.textContent = 'Git: cloning, resolve conflict';
+};
+
+changeButton.addEventListener('click', changeBackground);
+changeTitleBtn.addEventListener('click', changeTitle);
