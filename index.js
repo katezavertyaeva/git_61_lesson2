9881d29a4f38f2a35344dcc7d71826ef223ff8f8
Dmitriy Push <777pdn17@gmail.com>
2025-05-21 19:35:06 +0200

    again push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const changeButton = document.querySelector(".button-change");
 const root = document.querySelector("#root");
 
 const changeBackground = () => {
-  root.style.background = "rgb(238, 240, 169)";
+  root.style.background = "rgb(162, 123, 255)";
 };
 
 changeButton.addEventListener("click", changeBackground);
